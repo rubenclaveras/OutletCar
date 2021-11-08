cc95cd7035192fbe6e35be38bb47ee0f95a3e78e
rubenclaveras <rubenclaveras@opendeusto.es>
2021-11-08 15:45:42 +0100

    Creacion del modelo y migracion inicial

--- appOutletCar/models.py
+++ appOutletCar/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Coche(models.Model):
+    id= models.AutoField(primary_key= True)
+    marca= models.CharField(max_length= 15)
+    modelo= models.CharField(max_length= 35)
+    kilometros= models.IntegerField()
+    categoria= models.CharField(max_length= 300)
+    color= models.CharField(max_length= 20)
+    precio= models.IntegerField()
