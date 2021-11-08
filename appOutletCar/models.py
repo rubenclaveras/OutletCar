3a89667f96af80fd15d8cb3bba822d193fe1eda3
rubenclaveras <rubenclaveras@opendeusto.es>
2021-11-08 16:07:11 +0100

    Carga en BD 1

--- appOutletCar/models.py
+++ appOutletCar/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.db.models.fields.reverse_related import ManyToOneRel
 
 class Coche(models.Model):
     id= models.AutoField(primary_key= True)
     marca= models.CharField(max_length= 15)
     modelo= models.CharField(max_length= 35)
     kilometros= models.IntegerField()
-    categoria= models.CharField(max_length= 300)
+    categoria= models.CharField (max_length= 300)
     color= models.CharField(max_length= 20)
     precio= models.IntegerField()
