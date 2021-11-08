a7391cf80e8334e626e8e32f0bf0d4d7ee9b1796
rubenclaveras <rubenclaveras@opendeusto.es>
2021-11-08 13:57:53 +0100

    Corrección de errores 1

--- appOutletCar/views.py
+++ appOutletCar/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Listado de departamentos")
+    return HttpResponse("Esto marcha chavalees")
--- outletCar/settings.py
+++ outletCar/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'appOutletCar'
+    'appOutletCar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
