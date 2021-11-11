f5c7e7ca945481d5b705cbc7cd57e972738806e6
rubenclaveras <rubenclaveras@opendeusto.es>
2021-11-11 22:58:41 +0100

    Medios estaticos, css y js

--- outletCar/settings.py
+++ outletCar/settings.py
@@ -127,4 +127,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'fotos')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/fotos')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
