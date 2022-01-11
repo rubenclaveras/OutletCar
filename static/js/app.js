02828571e9c0bc16aa1a68f61cfed9424758d3d5
victormtzliz <victormtzliz@opendeusto.es>
2022-01-11 18:59:54 +0100

    corregir fallos

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,13 @@
-var check = document.querySelector(".check");
-check.addEventListener('clic', idioma);
+$(document).ready(function() {
+    var check = document.querySelector(".check");
+    check.addEventListener('clic', idioma);
 
-function idioma() {
-    let id = check.checked;
-    if (id == true) {
-        location.href = "ingles/index.html";
-    } else {
-        location.href = "../templates/indexhtml";
+    function idioma() {
+        let id = check.checked;
+        if (id == true) {
+            location.href = "../../ingles/index.html";
+        } else {
+            location.href = "../../templates/indexhtml";
+        }
     }
-}
+})
