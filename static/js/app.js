f3ef544a7bf3e8244aebc72f25ad41aef3aef5ed
Julen Alboni <julen.albonigamenor@opendeusto.es>
2022-01-11 15:27:28 +0100

    enlaces multilinguismo

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+var check = document.querySelector(".check");
+check.addEventListener('clic', idioma);
+
+function idioma() {
+    let id = check.checked;
+    if (id == true) {
+        location.href = "ingles/index.html";
+    } else {
+        location.href = "../templates/indexhtml";
+    }
+}
