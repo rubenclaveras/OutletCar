804c7328079f64db764c81fd7cad0a6101c6e315
victormtzliz <victormtzliz@opendeusto.es>
2022-01-12 17:03:27 +0100

    darkModeMedioAcabado

--- static/js/app.js
+++ static/js/app.js
@@ -3,17 +3,31 @@ window.addEventListener('load', () => {
     contenedor_loader.style.opacity = 0
     contenedor_loader.style.visibility = 'hidden'
 })
+
+/*var check1 = document.querySelector(".checkMode");
+check1.addEventListener('click', modoNocheDia);
+
+function modoNocheDia() {
+    let idi = check1.checked;
+    if (idi == true) {
+        $('.theme').attr('href', 'static/styles/dark.css');
+    } else {
+        $('.theme').attr('href', 'static/styles/estilos.css');
+    }
+}*/
 $(document).ready(function() {
     //Modificar el CSS de la class cabecera metiendo un borde negro
     $(".cabecera").css("border", "3px solid black");
+    //Modo nocturno
+    $('.checkMode').click(function() {
+        if ($('input.checkMode').is(':checked')) {
+            $('.theme').attr('href', '../static/styles/dark.css');
+        } else {
+            $('.theme').attr('href', '../static/styles/estilos.css');
+        }
+    })
 })
-$(".checkMode").click(function() {
-    if ($("input.checkMode").is(":checked")) {
-        $(".theme").attr("href", "styles/dark.css");
-    } else {
-        $(".theme").attr("href", "styles/estilos.css");
-    }
-})
+
 var check = document.querySelector(".check");
 check.addEventListener('click', idioma);
 
